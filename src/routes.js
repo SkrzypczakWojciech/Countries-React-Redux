b5bfe36b5d7a327126d58ccc0bb48036048cd204
Wojciech Skrzypczak <skrzypczakwojciech@wp.pl>
2018-09-18 13:29:39 +0200

    add coutry list component @ delete function

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,17 @@ import { Route, IndexRoute } from 'react-router';
 import Navigation from './presentational/navigation.component';
 import Home from './presentational/home.component';
 import Contact from './presentational/contact.component';
+import CountryFlagContainer from './containers/flag-container.component';
+import CountryDetailsContainer from './containers/country-detail-container.component';
+
 
 export default (
     <Route path='/' component={Navigation}>
-    	<IndexRoute component={Home}/>
-    	<Route path ='Contact' component={Contact}/>
+        <IndexRoute component={Home}/>
+        <Route path='countries' >
+            <IndexRoute component={CountryFlagContainer}/>
+            <Route path='country/:id' component={CountryDetailsContainer}/>
+        </Route>
+        <Route path='contact' component={Contact}/>
     </Route>
 );
